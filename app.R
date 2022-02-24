981651fb58c29bca872d7c3047fd952ace9f61e6
Steve Powell <steve@pogol.net>
2022-02-24 17:43:52 +0000

    fixes

--- app.R
+++ app.R
@@ -37,7 +37,8 @@ tags$head(
 ),
 h1("Search multiple pdf files"),
 p("Select more than one file (if required) by pressing shift, cmd or control when selecting. Then wait until upload is finished."),
-p("Caveat: this tool may not find all text within the PDF, and sometimes it may mangle the text if it is laid out in columns."),
+p("Caveat: this tool produces quick but not beautiful search results!"),
+p("It may not find all text within the PDF, and sometimes it may mangle the text if it is laid out in columns."),
 # p("You will then be able to download an Excel-format spreadsheet with all the text data in a single column, with one row for each of the original text areas in the documents, together with columns for absolute (not necessarily printed) page number and a column for document name."),
     fileInput("upload_upload",icon("upload") %>% span(" Upload"),
               multiple=T,
@@ -45,12 +46,13 @@ p("Caveat: this tool may not find all text within the PDF, and sometimes it may 
               placeholder="Drag files here or press Browse",
               accept = c("application/pdf")),
 
-div(id="down","Upload finished") %>% hidden,
+div(id="down",p("Upload finished"),
 h3("Search"),
 p("You can use | to separate different search terms."),
 textInput("search_text",NULL,placeholder = "Search text"),
 h3("Results"),
-div(id="results")
+div(id="results"),
+) %>% hidden
 # ,
 # div("Free service by ",a("Causal Map",href="https://causalmap.app"),style="position:absolute;bottom:60px;left:60px")
 # ,rpdfOutput("pdf", width = "100%", height = "400px")
